f4afb6a3a3e51aba4a948af7d376ce9e36ba7ef0
Prieto Cristian <pcristiandev@gmail.com>
2025-08-18 10:27:09 -0300

    Agregue la clase Comida al proyecto

---
+++ src/main/java/seguimiento_comida_gastos/dominio/Comida.java
@@ -0,0 +1,85 @@
+package seguimiento_comida_gastos.dominio;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Comida {
+    private int id;
+    private int precio;
+    private String nombre;
+    private Date fechaDeConsumo;
+    
+    public Comida(){}
+
+    // Se utilizara para eliminar una comidaw
+    public Comida(int id){
+        this.id = id;
+    }
+
+    // Se utilizara para crear una comida. El ID no se pasa porque lo maneja la base de datos
+    public Comida(int precio, String nombre, Date fechaDeConsumo){
+        this.precio = precio;
+        this.fechaDeConsumo = fechaDeConsumo;
+        this.nombre = nombre;
+    }
+
+    // Este constructor se va a usar para modificar. El usuario debe pasarnos el ID
+    public Comida (int id, int precio, String nombre, Date fechaDeConsumo){
+        this(precio, nombre, fechaDeConsumo);
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Comida{" +
+                "ID: " + id +
+                ", Precio: " + precio +
+                ", Nombre: '" + nombre + '\'' +
+                ", Fecha: " + fechaDeConsumo +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Comida comida = (Comida) o;
+        return id == comida.id && precio == comida.precio && Objects.equals(nombre, comida.nombre) && Objects.equals(fechaDeConsumo, comida.fechaDeConsumo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, precio, nombre, fechaDeConsumo);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaDeConsumo() {
+        return fechaDeConsumo;
+    }
+
+    public void setFechaDeConsumo(Date fechaDeConsumo) {
+        this.fechaDeConsumo = fechaDeConsumo;
+    }
+}
