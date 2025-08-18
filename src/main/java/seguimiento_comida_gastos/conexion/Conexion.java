512e9125a9c101123a3a4c41acd6e03cbf6df55a
Prieto Cristian <pcristiandev@gmail.com>
2025-08-18 10:15:33 -0300

    Establece conexion con Base de datos
    Primero deber importar la dependencia jdbc en el
    pom.xml y luego recargar el proyecto maven (esto
    hara que el proyecto reconozca las clases e inter
    faces dentro). Luego se crea una Clase Conexion
    que se encargara de conectarnos con la base de
    datos.

---
+++ src/main/java/seguimiento_comida_gastos/conexion/Conexion.java
@@ -0,0 +1,20 @@
+package seguimiento_comida_gastos.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    public static Connection getConexion(){
+        var baseDatos = "seguimiento_comida_gastos_db";
+        var url = "jdbc:mysql://localhost:3306/" + baseDatos;
+        var usuario = "root";
+        var contrasenia = "password";
+
+        try(Connection conexion = DriverManager.getConnection(url, usuario, contrasenia);){
+            return conexion;
+        }catch (Exception e){
+            System.out.println("Error al conectarnos a la DB: " + e.getMessage());
+        }
+        return null;
+    }
+}
